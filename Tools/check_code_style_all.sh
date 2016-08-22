cffda8373b6616d7bc84ab5749878c227f12b740
Andreas Antener <antener_a@gmx.ch>
2016-08-22 23:20:27 +0200

    filtering files for code check seperately to enable fast use of git pre-commit hook to check code style

--- Tools/check_code_style_all.sh
+++ Tools/check_code_style_all.sh
@@ -8,28 +8,7 @@ fi
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-find src \
-    -path src/lib/DriverFramework -prune -o \
-    -path src/lib/ecl -prune -o \
-    -path src/lib/external_lgpl -prune -o \
-    -path src/lib/mathlib -prune -o \
-    -path src/lib/matrix -prune -o \
-    -path src/modules/attitude_estimator_ekf -prune -o \
-    -path src/modules/commander -prune -o \
-    -path src/examples/ekf_att_pos_estimator -prune -o \
-    -path src/examples/attitude_estimator_ekf -prune -o \
-    -path src/modules/navigator -prune -o \
-    -path src/modules/sdlog2 -prune -o \
-    -path src/modules/uavcan -prune -o \
-    -path src/modules/uavcan/libuavcan -prune -o \
-    -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) \
-    -not -name '*generated.h' \
-    -not -name '*uthash.h' \
-    -not -name '*utstring.h' \
-    -not -name '*utlist.h' \
-    -not -name '*utarray.h' \
-    -print0 | xargs -0 -n 1 -P 8 -I % ${DIR}/check_code_style.sh %
-
+${DIR}/files_to_check_code_style.sh | xargs -n 1 -P 8 -I % ${DIR}/check_code_style.sh %
 
 if [ $? -eq 0 ]; then
     echo "Format checks passed"
