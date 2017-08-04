PX4/PX4-Autopilot

Initialize all outgoing vehicle_command_ack_s and vehicle_command_s
This will initialize those structs with zero in all fields not set
and all fields set will only be change once to the final value not
wasting CPU time zeroing it.

This will guarantee that no non-unitialized structs will have
a trash value on from_external causing it to be sent to the
MAVLink channel without need it.

src/modules/navigator/navigator_main.cpp
@@ -377,7 +377,7 @@ Navigator::task_main()
 		orb_check(_vehicle_command_sub, &updated);
 
 		if (updated) {
-			vehicle_command_s cmd = {};
+			vehicle_command_s cmd;
 			orb_copy(ORB_ID(vehicle_command), _vehicle_command_sub, &cmd);
 
 			if (cmd.command == vehicle_command_s::VEHICLE_CMD_DO_REPOSITION) {
@@ -481,14 +481,20 @@ Navigator::task_main()
 				int land_start = _mission.find_offboard_land_start();
 
 				if (land_start != -1) {
-					vehicle_command_s vcmd = {};
-					vcmd.target_system = get_vstatus()->system_id;
-					vcmd.target_component = get_vstatus()->component_id;
-					vcmd.command = vehicle_command_s::VEHICLE_CMD_MISSION_START;
-					vcmd.param1 = land_start;
-					vcmd.param2 = 0;
+					struct vehicle_command_s vcmd = {
+						.timestamp = hrt_absolute_time(),
+						.param5 = 0.0f,
+						.param6 = 0.0f,
+						.param1 = (float)land_start,
+						.param2 = 0.0f,
+						.param3 = 0.0f,
+						.param4 = 0.0f,
+						.param7 = 0.0f,
+						.command = vehicle_command_s::VEHICLE_CMD_MISSION_START,
+						.target_system = (uint8_t)get_vstatus()->system_id,
+						.target_component = (uint8_t)get_vstatus()->component_id
+					};
 
-					vcmd.timestamp = hrt_absolute_time();
 					publish_vehicle_cmd(vcmd);
 
 				} else {

zehortigoza/Firmware

src/modules/navigator/navigator_main.cpp
@@ -375,7 +375,7 @@ Navigator::task_main()
 		orb_check(_vehicle_command_sub, &updated);
 
 		if (updated) {
-			vehicle_command_s cmd = {};
+			vehicle_command_s cmd;
 			orb_copy(ORB_ID(vehicle_command), _vehicle_command_sub, &cmd);
 
 			if (cmd.command == vehicle_command_s::VEHICLE_CMD_DO_REPOSITION) {
@@ -479,13 +479,19 @@ Navigator::task_main()
 				int land_start = _mission.find_offboard_land_start();
 
 				if (land_start != -1) {
-					vehicle_command_s cmd_mission_start = {};
-					cmd_mission_start.timestamp = hrt_absolute_time();
-					cmd_mission_start.target_system = get_vstatus()->system_id;
-					cmd_mission_start.target_component = get_vstatus()->component_id;
-					cmd_mission_start.command = vehicle_command_s::VEHICLE_CMD_MISSION_START;
-					cmd_mission_start.param1 = land_start;
-					cmd_mission_start.param2 = 0;
+					struct vehicle_command_s cmd_mission_start = {
+						.timestamp = hrt_absolute_time(),
+						.param5 = 0.0f,
+						.param6 = 0.0f,
+						.param1 = (float)land_start,
+						.param2 = 0.0f,
+						.param3 = 0.0f,
+						.param4 = 0.0f,
+						.param7 = 0.0f,
+						.command = vehicle_command_s::VEHICLE_CMD_MISSION_START,
+						.target_system = (uint8_t)get_vstatus()->system_id,
+						.target_component = (uint8_t)get_vstatus()->component_id
+					};
 
 					publish_vehicle_cmd(cmd_mission_start);
 